b4ffb85f2aa8138700e1580a334e95e0064618a5
Lars Pind <lars@pinds.com>
2010-10-15 00:41:48 +0200

    Trying with mailer-mixin

---
+++ lib/dj_remixes/action_mailer/action_mailer.rb
@@ -0,0 +1,28 @@
+module Mail
+  class Message
+    
+    class MailmanWorker < DJ::Worker
+      priority :urgent
+      run_at {1.year.ago}
+
+      def perform   
+        # Force loading of the class first to avoid the dreaded 'undefined class/module' error
+        eval(self.klass)
+        message = Marshal.load(self.mail)
+        message.deliver_without_worker
+      end
+    end # MailmanWorker
+    
+    def deliver_with_worker
+      if ActionMailer::Base.delivery_method == :test
+        deliver_without_worker
+      else
+        Mail::Message::MailmanWorker.enqueue(:mail => Marshal.dump(self), :klass => self.class.name)
+        return self
+      end
+    end
+    
+    alias_method_chain :deliver, :worker
+    
+  end # Message
+end # Mail
